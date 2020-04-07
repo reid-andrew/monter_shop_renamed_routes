22e02a1195b7c805b9465db22f227f1da8da7f38
Alex Lynch <mlynch5187@gmail.com>
2020-04-07 17:09:19 -0600

    Complete user story 5

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -20,6 +20,15 @@ RSpec.describe "test users index", type: :feature do
                 email: "test1@turing.com",
                 password_digest: "123456",
                 role: 1)
+
+    @admin = User.create(name: "Mike Dao",
+                street_address: "1765 Larimer St",
+                city: "Denver",
+                state: "CO",
+                zip: "80202",
+                email: "test2@turing.com",
+                password_digest: "123456",
+                role: 2)
   end
 
     it "shows links to home, all merchants, all items, profile, logout, and no links to login or register" do
@@ -51,6 +60,23 @@ RSpec.describe "test users index", type: :feature do
 
       expect(page).to_not have_link("Login")
       expect(page).to_not have_link("Register")
+  end
+
+  it "shows admins same links as a regular user plus links to admin dashboard and all users page" do
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit "/"
+
+    expect(page).to have_link("All Merchants")
+    expect(page).to have_link("All Items")
+    expect(page).to have_link("Profile")
+    expect(page).to have_link("Logout")
+    expect(page).to have_link("Admin Dashboard")
+    expect(page).to have_link("All Users")
+
+    expect(page).to_not have_link("Login")
+    expect(page).to_not have_link("Register")
   end
 end
 
