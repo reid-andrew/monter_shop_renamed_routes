3f12c42ada8812ec1a1e69b3a7e04573f82ab854
Andrew Reid <areid.sj@gmail.com>
2020-04-15 14:35:29 -0600

    Reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,15 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :merchants
+  resources :items, only: [:index, :show, :edit, :update, :destroy]
+  resources :reviews, only: [:edit, :update, :destroy]
 
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
 
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
-
-  resources :reviews, only: [:edit, :update, :destroy]
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
