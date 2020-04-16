b968f23908fa742e368323e184732282376b8f83
Andrew Reid <areid.sj@gmail.com>
2020-04-16 08:42:57 -0600

    Further refactor of item orders update method

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -8,9 +8,7 @@ class ItemOrdersController <ApplicationController
     @item_order = ItemOrder.find(item_order_params[:item_order_id])
     if item_order_params[:type] == "fulfill"
       @item_order.update(:status => "Fulfilled")
-      total_items = @item_order.order.item_orders
-      fulfilled_items = total_items.where("status = 'Fulfilled'")
-      if total_items.count == fulfilled_items.count
+      if @item_order.order.item_orders.count == @item_order.order.item_orders.where("status = 'Fulfilled'").count
         @item_order.order.update(:status => "Packaged")
       end
     end
